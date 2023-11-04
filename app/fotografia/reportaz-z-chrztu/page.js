7f0cf16852c6cf4cdc4c37938469fbf0a94c5f63
Valkon <96625039+Nawija@users.noreply.github.com>
2023-11-04 14:58:34 +0100

    Update page.js

--- app/fotografia/reportaz-z-chrztu/page.js
+++ app/fotografia/reportaz-z-chrztu/page.js
@@ -1,4 +1,5 @@
 import { notFound } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const fetchTodo = async () => {
     const res = await fetch("https://graphql.datocms.com/", {
@@ -18,25 +19,25 @@ const fetchTodo = async () => {
 };
 
 export default async function ReportazChrzestPage() {
-    const datocmsResponse = await fetchTodo();
+    const [results, setResults] = useState([]);
+    const controller = new AbortController();
 
-    if (!datocmsResponse) return notFound();
+    useEffect(() => {
+        async function getData() {
+            const res = await fetch("https://graphql.datocms.com/", {
+                next: { revalidate: 60 },
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json",
+                    Authorization: `Bearer ${process.env.NEXT_DATOCMS_API_TOKEN}`,
+                },
+                body: JSON.stringify({
+                    query: "{ allReportazZChrztus { img { id url } } }",
+                }),
+            });
+        }
+    });
 
-    const photos = datocmsResponse.data.allReportazZChrztus;
-
-    return (
-        <div className="flex flex-wrap ml-3 mt-4">
-            {photos.map((photoCollection) =>
-                photoCollection.img.map((photo) => (
-                    <img
-                        className="p-1 object-cover h-72 opacityAnimation"
-                        height={250}
-                        width={260}
-                        key={photo.id}
-                        src={photo.url}
-                    />
-                ))
-            )}
-        </div>
-    );
+    return <>xxxx</>;
 }
