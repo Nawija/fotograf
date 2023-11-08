489e11e4775a52ad129cf02975c063da5abf848a
Valkon <96625039+Nawija@users.noreply.github.com>
2023-11-08 13:04:32 +0100

    sdffdsf

--- app/fotografia/fotografia-slubna/[photo]/page.js
+++ app/fotografia/fotografia-slubna/[photo]/page.js
@@ -8,47 +8,29 @@ export default async function ReportazChrzestPage(photo) {
 
     const photos = datoCmsRes.data.allReportazZChrztus;
 
-    const LinkMenu =
-        "bg-gray-500/80 rounded-xl text-[12.5px] font-medium py-1.5 px-3 text-white hover:bg-gray-500 transition-colors";
+    const selectedPhoto = photos.find((photoCollection) =>
+        photoCollection.img.some((photo) => photo.id === photo.id)
+    );
+
+    if (!selectedPhoto) {
+        return notFound();
+    }
+
+    const selectedPhotoUrl = selectedPhoto.img[2].url;
 
     return (
         <div className="ml-3 mt-3">
-            <div className="text-end mr-12 space-x-3 mb-2">
-                <button className={LinkMenu}>Pobierz</button>
-                <button className={LinkMenu}>Pobierz Tylko Ulubione</button>
-            </div>
-            <div className="flex flex-wrap ">
-                {photos.map((photoCollection) =>
-                    photoCollection.img.map((photo) => (
-                        <Link
-                            href={`/fotografia/fotografia-slubna/${photo.id}`}
-                            className="relative m-3 group transition-all duration-200 hover:scale-[1.02] hover:shadow-2xl cursor-pointer overflow-hidden rounded-md"
-                        >
-                            <img
-                                className="object-cover h-72 opacityAnimation"
-                                width={230}
-                                key={photo.id}
-                                src={photo.url}
-                            />
-                            <div className="opacity-0 group-hover:opacity-100 absolute h-full w-full top-0 left-0 bg-gradient-to-t from-black/40 via-transparent to-transparent transition-all duration-500 delay-100">
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke-width="2"
-                                    stroke="currentColor"
-                                    className="w-6 h-6 absolute bottom-2 right-2 text-white"
-                                >
-                                    <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-                                    />
-                                </svg>
-                            </div>
-                        </Link>
-                    ))
-                )}
+            <div className="flex items-center justify-center">
+                <div className="flex p-10 items-center justify-center fixed top-0 left-0 w-screen h-screen z-50">
+                <div className="bg-gray-900/80 w-full h-full absolute -z-10 backdrop-blur-sm" />
+                    <Link href={`/fotografia/fotografia-slubna/` + selectedPhoto.img[0].id} className="px-3 py-1.5 bg-black text-white font-semibold w-max h-max m-2 rounded-full">a</Link>
+                    <img
+                        className="object-cover opacityAnimation"
+                        key={selectedPhoto.img.id}
+                        src={selectedPhotoUrl}
+                    />
+                    <Link href={`/fotografia/fotografia-slubna/` + selectedPhoto.img[0].id} className="px-3 py-1.5 bg-black text-white font-semibold w-max h-max m-2 rounded-full">e</Link>
+                </div>
             </div>
         </div>
     );
--- app/fotografia/reportaz-z-chrztu/page.js
+++ app/fotografia/reportaz-z-chrztu/page.js
@@ -1,15 +1,22 @@
-"use client";
+import { notFound } from "next/navigation";
+import Action from "./action";
 
-import { useQuery, gql } from "@apollo/client";
-import { getClient } from "@apollo/client";
-
-const GET_BOOKS = gql`
-allReportazZChrztus { img { id url } } ",
-`;
-
-function BookList() {
-    const client = getClient();
-    const { loading, data } = useQuery(GET_BOOKS, { client });
-
-    if (loading) return <p>loading...</p>;
+export default async function XX({ photos }) {
+    const res = await fetch("https://graphql.datocms.com/", {
+        next: { tags: ["collection"] },
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: `Bearer ${process.env.NEXT_DATOCMS_API_TOKEN}`,
+        },
+        body: JSON.stringify({
+            query: "{ allReportazZChrztus { img { id url } } }",
+        }),
+    });
+    const datoCmsRes = await res.json();
+    if (!datoCmsRes) return notFound();
+    photos = datoCmsRes.data.allReportazZChrztus;
+    
+    return <Action photos={photos} />;
 }
