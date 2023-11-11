e37cd2217e1f3d3d84c5c049d7bdca898f50a33e
Valkon <96625039+Nawija@users.noreply.github.com>
2023-11-11 09:58:09 +0100

    Update layout.js

--- app/fotografia/layout.js
+++ app/fotografia/layout.js
@@ -1,10 +1,6 @@
-"use client";
-import { useState } from "react";
 import Categories from "./Categories";
 
 export default function RootLayout({ children }) {
-    const [showMenu, setShowMenu] = useState(false);
-
     return (
         <main className="flex max-w-screen-2xl mx-auto">
             <div>
