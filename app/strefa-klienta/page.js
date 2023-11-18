f2509d05c74dd1b426849a2b718ae75fba4332eb
Valkon <96625039+Nawija@users.noreply.github.com>
2023-11-18 16:11:07 +0100

    Update page.js

--- app/strefa-klienta/page.js
+++ app/strefa-klienta/page.js
@@ -20,7 +20,7 @@ const fetchPhotoDatoCms = async () => {
 
 const fetchLikesDB = async () => {
     try {
-        const res = await fetch("/api/strefa-klienta", {
+        const res = await fetch("https://x1-git-main-nawija.vercel.app/api/strefa-klienta", {
             method: "GET",
             cache: "no-store",
         });
