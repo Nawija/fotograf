4bcaaad20998a62a790e9900cf4150ddb6e71ad7
Valkon <96625039+Nawija@users.noreply.github.com>
2023-11-20 08:39:14 +0100

    Update layout.js

--- app/fotografia-slubna/layout.js
+++ app/fotografia-slubna/layout.js
@@ -1,22 +1,12 @@
-"use client";
-import { useState } from "react";
 import Categories from "./Categories";
 
 export default function RootLayout({ children }) {
-    const [showMenu, setShowMenu] = useState(false);
-
     return (
         <main className="flex max-w-screen-2xl mx-auto mb-20">
             <div>
-                <Categories showMenu={showMenu} setShowMenu={setShowMenu} />
+                <Categories />
             </div>
-            <div
-                showMenu={showMenu}
-                setShowMenu={setShowMenu}
-                className="flex-1"
-            >
-                {children}
-            </div>
+            <div className="flex-1">{children}</div>
         </main>
     );
 }
