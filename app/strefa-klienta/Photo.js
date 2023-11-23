3383474dcfcec7d3920c6897948d08533139325f
Valkon <96625039+Nawija@users.noreply.github.com>
2023-11-23 17:24:14 +0100

    Update Photo.js

--- app/strefa-klienta/Photo.js
+++ app/strefa-klienta/Photo.js
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
-
 import { useEffect, useState } from "react";
+import ImageGallery from "react-image-gallery";
 
 export default function Photo({ FotografiaSlubna, Likes }) {
     let photos = FotografiaSlubna.data.allAa121223s[0].img;
@@ -51,57 +51,86 @@ export default function Photo({ FotografiaSlubna, Likes }) {
             }
         }
     };
+    const images = [
+        {
+            original: "https://picsum.photos/id/1018/1000/600/",
+            thumbnail: "https://picsum.photos/id/1018/250/150/",
+        },
+        {
+            original: "https://picsum.photos/id/1015/1000/600/",
+            thumbnail: "https://picsum.photos/id/1015/250/150/",
+        },
+        {
+            original: "https://picsum.photos/id/1019/1000/600/",
+            thumbnail: "https://picsum.photos/id/1019/250/150/",
+        },
+        {
+            original: "https://picsum.photos/id/1018/1000/600/",
+            thumbnail: "https://picsum.photos/id/1018/250/150/",
+        },
+        {
+            original: "https://picsum.photos/id/1015/1000/600/",
+            thumbnail: "https://picsum.photos/id/1015/250/150/",
+        },
+        {
+            original: "https://picsum.photos/id/1019/1000/600/",
+            thumbnail: "https://picsum.photos/id/1019/250/150/",
+        },
+    ];
 
     return (
-        <div className="flex flex-wrap opacityAnimation">
-            {photos.map((photo) => (
-                <div className="relative group mx-3 my-2 lg:m-2">
-                    <div
-                        key={photo.id}
-                        className={`relative transition-all w-full h-full lg:h-56 lg:w-60 duration-200 lg:hover:scale-[1.015] hover:shadow-2xl cursor-pointer overflow-hidden rounded-md`}
-                    >
-                        <Link
-                            href={`/strefa-klienta/${photo.id}`}
+        <>
+            <ImageGallery items={images} />
+            <div className="flex flex-wrap opacityAnimation">
+                {photos.map((photo) => (
+                    <div className="relative group mx-3 my-2 lg:m-2">
+                        <div
                             key={photo.id}
+                            className={`relative transition-all w-full h-full lg:h-56 lg:w-60 duration-200 lg:hover:scale-[1.015] hover:shadow-2xl cursor-pointer overflow-hidden rounded-md`}
                         >
-                            <img
-                                className="object-cover opacityAnimation h-full w-full"
-                                src={photo.url}
-                            />
-                            <div className="opacity-0 group-hover:opacity-100 absolute h-full w-full top-0 left-0 bg-gradient-to-b to-black/70 via-transparent from-transparent transition-all duration-500 delay-100 lg:flex hidden" />
-                        </Link>
-                        <button
-                            className="absolute bottom-6 lg:bottom-4 right-2 lg:right-4 z-40 w-max"
-                            onClick={() => handleLike(photo.id, photo.url)}
-                        >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                                strokeWidth="1.5"
-                                stroke="currentColor"
-                                className={`w-9 h-9 p-1.5  z-20 ${
-                                    likedPhotos.includes(photo.id)
-                                        ? "text-red-400/80 fill-red-500/80"
-                                        : "text-white/40 fill-white/20"
-                                }`}
+                            <Link
+                                href={`/strefa-klienta/${photo.id}`}
+                                key={photo.id}
                             >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                                <img
+                                    className="object-cover opacityAnimation h-full w-full"
+                                    src={photo.url}
                                 />
-                            </svg>
-                            <div
-                                className={`absolute top-0 blur-lg -z-10 w-4 h-4 p-1.5  transition-all duration-300 ${
-                                    likedPhotos.includes(photo.id)
-                                        ? "bg-red-600"
-                                        : ""
-                                }`}
-                            />
-                        </button>
+                                <div className="opacity-0 group-hover:opacity-100 absolute h-full w-full top-0 left-0 bg-gradient-to-b to-black/70 via-transparent from-transparent transition-all duration-500 delay-100 lg:flex hidden" />
+                            </Link>
+                            <button
+                                className="absolute bottom-6 lg:bottom-4 right-2 lg:right-4 z-40 w-max"
+                                onClick={() => handleLike(photo.id, photo.url)}
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth="1.5"
+                                    stroke="currentColor"
+                                    className={`w-9 h-9 p-1.5  z-20 ${
+                                        likedPhotos.includes(photo.id)
+                                            ? "text-red-400/80 fill-red-500/80"
+                                            : "text-white/40 fill-white/20"
+                                    }`}
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                                    />
+                                </svg>
+                                <div
+                                    className={`absolute top-0 blur-lg -z-10 w-4 h-4 p-1.5  transition-all duration-300 ${
+                                        likedPhotos.includes(photo.id)
+                                            ? "bg-red-600"
+                                            : ""
+                                    }`}
+                                />
+                            </button>
+                        </div>
                     </div>
-                </div>
-            ))}
-        </div>
+                ))}
+            </div>
+        </>
     );
 }
